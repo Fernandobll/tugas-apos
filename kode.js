f8fb9764e724e1e9a121a6a774ccf88331294233
Fernandobll <137286983+Fernandobll@users.noreply.github.com>
2023-06-22 15:55:32 +0700

    Add files via upload

---
+++ kode.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+   //ketika submit pada input button
+   
+   //lanjutan setelah submit
+   var Nama = $ ("#Nama").val();
+   $("#a1").html("Fernando Obinizaro Buulolo");
+
+   var Jurusan = $ ("#Jurusan").val();
+   $("#a2").html("Yunda Srisusanti");
+
+   var Kelas = $ ("#Kelas").val();
+   $("#a3").html("Sukron Roby P");
+
+   var Nama = $ ("#Nama").val();
+   $("#a4").html("Deya Anggi Saskia");
+
+});
+
